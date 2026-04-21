9fba11d386107459aff431bd8e53622db4074bdf
Fracta-Axis <mf410360@gmail.com>
2026-04-21 11:35:10 -0300

    Actualizar app.py

--- app.py
+++ app.py
@@ -912,7 +912,7 @@ def main():
     """, unsafe_allow_html=True)
 
     # ── Header ───────────────────────────────────────────────────────────────
-    st.markdown('<div class="main-title">🌀 Axis</div>',
+    st.markdown('<div class="main-title">🌀 Fractalyx</div>',
                 unsafe_allow_html=True)
     st.markdown('<div class="sub"> Una arquitectura original que combina dinámica fractal-estocástica con criptografía moderna (SHA3-256, HMAC, KDF)."    </div>',
                 unsafe_allow_html=True)
